a07eb7894fbc0e6375d023d15c315579dc1880d1
maxime <maxime.cahu@yahoo.com>
2023-02-10 12:34:15 +0100

    added rounds and scores

--- script.js
+++ script.js
@@ -2,34 +2,59 @@
 User inputs choice of rock or paper or scissors
 Computer randomly selects an option from rock paper scissors
 if userInput == computerInput then no winner
-if userInput = rock and computerInput = scissors then user wins
-if userInput = paper and computerInput = rock then user wins
-if userInput = scissors and computerInput = paper then user wins
+if userInput == rock and computerInput = scissors then user wins
+if userInput == paper and computerInput = rock then user wins
+if userInput == scissors and computerInput = paper then user wins
 else computer wins
 */
+let userScore = 0;
+let computerScore = 0;
+for (let i = 1; i < 6; i++) {
+    console.log("ROUND " + i + "\!")
+    userInput = prompt("Choose rock, paper, or scissors");
 
-userInput = prompt("Choose rock, paper, or scissors");
-
-if (userInput.toLowerCase() !== "rock" && userInput.toLowerCase() !== "paper" && userInput.toLowerCase() !== "scissors") {
-    console.log("Invalid input");
-} else {
-
-    const computerChoices = ["rock", "paper", "scissors"];
-
-    let computerInput = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-    console.log("Your choice: " + userInput)
-    console.log("Computer's choice: " + computerInput)
-
-    if (userInput == computerInput) {
-        console.log("No winners. Play again.");
-    } else if (userInput == "rock" && computerInput == "scissors") {
-        console.log("You won!");
-    } else if (userInput == "paper" && computerInput == "rock") {
-        console.log("You won!");
-    } else if (userInput == "scissors" && computerInput == "paper") {
-        console.log("You won!");
+    if (userInput.toLowerCase() !== "rock" && userInput.toLowerCase() !== "paper" && userInput.toLowerCase() !== "scissors") {
+        console.log("Invalid input\n\n");
+        i--;
     } else {
-        console.log("You lost!");
+
+        const computerChoices = ["rock", "paper", "scissors"];
+
+        let computerInput = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+        console.log("Your choice: " + userInput);
+        console.log("Computer's choice: " + computerInput + "\n");
+
+        if (userInput == computerInput) {
+            console.log("No winners. Play again.");
+            console.log("Your score: " + userScore)
+            console.log("Computer's choice: " + computerScore + "\n\n")
+        } else if (userInput == "rock" && computerInput == "scissors") {
+            console.log("You won this round!\n");
+            userScore += 1;
+            console.log("Your score: " + userScore)
+            console.log("Computer's choice: " + computerScore + "\n\n")
+        } else if (userInput == "paper" && computerInput == "rock") {
+            console.log("You won this round!\n");
+            userScore += 1;
+            console.log("Your score: " + userScore)
+            console.log("Computer's choice: " + computerScore + "\n\n")
+        } else if (userInput == "scissors" && computerInput == "paper") {
+            console.log("You won this round!\n");
+            userScore += 1;
+            console.log("Your score: " + userScore)
+            console.log("Computer's choice: " + computerScore + "\n\n")
+        } else {
+            console.log("You lost this round!\n");
+            computerScore += 1;
+            console.log("Your score: " + userScore)
+            console.log("Computer's choice: " + computerScore + "\n\n")
+        }
     }
+}
+
+if (userScore > computerScore) {
+    console.log("You won the game!")
+} else {
+    console.log("The machines will rule over the world!")
 }
