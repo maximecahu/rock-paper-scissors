55dd58ba395a3120b5e11045f321f3a92af2b1a5
maxime <maxime.cahu@yahoo.com>
2023-02-12 22:15:53 +0100

    very nice looking

--- script.js
+++ script.js
@@ -7,15 +7,24 @@ if userInput == paper and computerInput = rock then user wins
 if userInput == scissors and computerInput = paper then user wins
 else computer wins
 */
-let userScore = 0;
-let computerScore = 0;
 
 
-if (userInput.toLowerCase() !== "rock" && userInput.toLowerCase() !== "paper"
-    && userInput.toLowerCase() !== "scissors") {
-    console.log("Invalid input\n\n");
-    i--;
-} else {
+let userScore = 4;
+let computerScore = 4;
+let roundNumber = 1;
+let imgElement = document.getElementById("imageVS");
+
+function play(userInput) {
+
+    const div = document.querySelector(".imageVS");
+
+    div.classList.remove("imageVS");
+    div.classList.add("shaking-imageVS");
+
+    setTimeout(function () {
+        div.classList.remove("shaking-imageVS");
+        div.classList.add("imageVS");
+    }, 500);
 
     const computerChoices = ["rock", "paper", "scissors"];
 
@@ -24,38 +33,74 @@ if (userInput.toLowerCase() !== "rock" && userInput.toLowerCase() !== "paper"
     console.log("Your choice: " + userInput);
     console.log("Computer's choice: " + computerInput + "\n");
 
-    if (userInput == computerInput) {
-        console.log("No winners. Play again.");
-        console.log("Your score: " + userScore)
-        console.log("Computer's choice: " + computerScore + "\n\n")
-    } else if (userInput == "rock" && computerInput == "scissors") {
-        console.log("You won this round!\n");
-        userScore += 1;
-        console.log("Your score: " + userScore)
-        console.log("Computer's choice: " + computerScore + "\n\n")
-    } else if (userInput == "paper" && computerInput == "rock") {
-        console.log("You won this round!\n");
-        userScore += 1;
-        console.log("Your score: " + userScore)
-        console.log("Computer's choice: " + computerScore + "\n\n")
-    } else if (userInput == "scissors" && computerInput == "paper") {
-        console.log("You won this round!\n");
-        userScore += 1;
-        console.log("Your score: " + userScore)
-        console.log("Computer's choice: " + computerScore + "\n\n")
-    } else {
-        console.log("You lost this round!\n");
-        computerScore += 1;
-        console.log("Your score: " + userScore)
-        console.log("Computer's choice: " + computerScore + "\n\n")
+    document.getElementById('imageYou')
+        .src = `images/${userInput}.png`;
+
+    document.getElementById('imagePc')
+        .src = `images/${computerInput}2.png`;
+
+
+    setTimeout(function () {
+
+
+
+        if (userInput == computerInput) {
+            roundNumber += 1;
+            console.log("No winners. Play again.");
+            console.log("Your score: " + userScore)
+            console.log("Computer's choice: " + computerScore + "\n\n")
+        } else if (userInput == "rock" && computerInput == "scissors") {
+            console.log("You won this round!\n");
+            userScore += 1;
+            roundNumber += 1;
+            console.log("Your score: " + userScore)
+            console.log("Computer's choice: " + computerScore + "\n\n")
+
+        } else if (userInput == "paper" && computerInput == "rock") {
+            console.log("You won this round!\n");
+            userScore += 1;
+            roundNumber += 1;
+            console.log("Your score: " + userScore)
+            console.log("Computer's choice: " + computerScore + "\n\n")
+
+        } else if (userInput == "scissors" && computerInput == "paper") {
+            console.log("You won this round!\n");
+            userScore += 1;
+            roundNumber += 1;
+            console.log("Your score: " + userScore)
+            console.log("Computer's choice: " + computerScore + "\n\n")
+
+        } else {
+            console.log("You lost this round!\n");
+            computerScore += 1;
+            roundNumber += 1;
+            console.log("Your score: " + userScore)
+            console.log("Computer's choice: " + computerScore + "\n\n")
+
+        }
+        document.getElementById('scoreYou')
+            .innerHTML = `${userScore}`;
+        document.getElementById('scorePc')
+            .innerHTML = `${computerScore}`;
+        document.getElementById('round')
+            .innerHTML = `Round ${roundNumber}!`;
+
+
+
+    }, 2000);
+
+    if (userScore === 5) {
+        imgElement.src = "images/youWin.png";
+
+
+
+    } else if (computerScore === 5) {
+        imgElement.src = "images/youLose.png";
+
     }
+
+
+
 }
 
 
-if (userScore > computerScore) {
-    console.log("You won the game!")
-} else if (userScore < computerScore) {
-    console.log("The machines will rule over the world!")
-} else {
-    console.log("The computer hacked the game and added 1 point to its score. The computer won the game. The machines will rules over the world!")
-}
