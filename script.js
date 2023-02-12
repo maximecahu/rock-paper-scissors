fdb2b81e2f1f0649cdc1392b6012cbaec7abd6f8
maxime <maxime.cahu@yahoo.com>
2023-02-12 17:47:54 +0100

    added css

--- script.js
+++ script.js
@@ -9,52 +9,53 @@ else computer wins
 */
 let userScore = 0;
 let computerScore = 0;
-for (let i = 1; i < 6; i++) {
-    console.log("ROUND " + i + "\!")
-    userInput = prompt("Choose rock, paper, or scissors");
 
-    if (userInput.toLowerCase() !== "rock" && userInput.toLowerCase() !== "paper" && userInput.toLowerCase() !== "scissors") {
-        console.log("Invalid input\n\n");
-        i--;
+
+if (userInput.toLowerCase() !== "rock" && userInput.toLowerCase() !== "paper"
+    && userInput.toLowerCase() !== "scissors") {
+    console.log("Invalid input\n\n");
+    i--;
+} else {
+
+    const computerChoices = ["rock", "paper", "scissors"];
+
+    let computerInput = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    console.log("Your choice: " + userInput);
+    console.log("Computer's choice: " + computerInput + "\n");
+
+    if (userInput == computerInput) {
+        console.log("No winners. Play again.");
+        console.log("Your score: " + userScore)
+        console.log("Computer's choice: " + computerScore + "\n\n")
+    } else if (userInput == "rock" && computerInput == "scissors") {
+        console.log("You won this round!\n");
+        userScore += 1;
+        console.log("Your score: " + userScore)
+        console.log("Computer's choice: " + computerScore + "\n\n")
+    } else if (userInput == "paper" && computerInput == "rock") {
+        console.log("You won this round!\n");
+        userScore += 1;
+        console.log("Your score: " + userScore)
+        console.log("Computer's choice: " + computerScore + "\n\n")
+    } else if (userInput == "scissors" && computerInput == "paper") {
+        console.log("You won this round!\n");
+        userScore += 1;
+        console.log("Your score: " + userScore)
+        console.log("Computer's choice: " + computerScore + "\n\n")
     } else {
-
-        const computerChoices = ["rock", "paper", "scissors"];
-
-        let computerInput = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-        console.log("Your choice: " + userInput);
-        console.log("Computer's choice: " + computerInput + "\n");
-
-        if (userInput == computerInput) {
-            console.log("No winners. Play again.");
-            console.log("Your score: " + userScore)
-            console.log("Computer's choice: " + computerScore + "\n\n")
-        } else if (userInput == "rock" && computerInput == "scissors") {
-            console.log("You won this round!\n");
-            userScore += 1;
-            console.log("Your score: " + userScore)
-            console.log("Computer's choice: " + computerScore + "\n\n")
-        } else if (userInput == "paper" && computerInput == "rock") {
-            console.log("You won this round!\n");
-            userScore += 1;
-            console.log("Your score: " + userScore)
-            console.log("Computer's choice: " + computerScore + "\n\n")
-        } else if (userInput == "scissors" && computerInput == "paper") {
-            console.log("You won this round!\n");
-            userScore += 1;
-            console.log("Your score: " + userScore)
-            console.log("Computer's choice: " + computerScore + "\n\n")
-        } else {
-            console.log("You lost this round!\n");
-            computerScore += 1;
-            console.log("Your score: " + userScore)
-            console.log("Computer's choice: " + computerScore + "\n\n")
-        }
+        console.log("You lost this round!\n");
+        computerScore += 1;
+        console.log("Your score: " + userScore)
+        console.log("Computer's choice: " + computerScore + "\n\n")
     }
 }
 
+
 if (userScore > computerScore) {
     console.log("You won the game!")
-} else {
+} else if (userScore < computerScore) {
     console.log("The machines will rule over the world!")
+} else {
+    console.log("The computer hacked the game and added 1 point to its score. The computer won the game. The machines will rules over the world!")
 }
